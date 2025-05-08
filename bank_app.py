49b6bac5aee4889046f4c76b18a2e85dfb42e1f9
Seeman Judson Josuva <UT010511@unicomtic.com>
2025-05-08 16:17:30 +0530

    All frames ready

--- bank_app.py
+++ bank_app.py
@@ -1,101 +1,148 @@
-#import datetime
 from datetime import datetime
 now=datetime.now()
-ac_balances=0
-''''accounts=[]
-auto_account_num=100001
-#Function for create the account
-def create_account():
-    global auto_account_num
-    global accounts
-    name=str(input("Enter the account holder name: "))
-    pin=(input("Enter the pin number: "))  
-    try:
-        initial_balance=float(input("Enter your first diposit amount: "))
-        if initial_balance <= 0:
-            print("Initioal balance is must be a positive....")
-            return 
-    except ValueError:
-        print("Invalid input.....")      
-        return
-    account_number=auto_account_num
-    auto_account_num += 1
-    accounts=[f'\n{account_number},{name},{pin},{initial_balance}']
-    with open("accounts.txt", "a") as file:
-         file.writelines(accounts)
-    #accounts[account_number]={"Name":name,"Balance":initial_balance,"Pin":pin}
-    #with open("accounts.txt", 'a+') as file:
-        #file.write(f'\n{str(accounts)}')
-         print(f"*****Succssesfully Created your account***** \n .....Thank you for Join with us.....")
-         print(f'Account holder name is: {name} \n Your account number is: {account_number}\n Now Your account balance is: {initial_balance}')
-        #print(accounts)
-create_account()
+ac_balances = 0
+admin_user_name = "Admin"
+admin_password = "Admin@123"
 
-def account_num_verification():
-    with open("accounts.txt", "r") as file:
-        for line in file.readlines([]):
-            line +=line
-            check=list(file.readlines([line]))
-        print(check)
-account_num_verification()        '''
-
-       #===GET DATA FROME USER==== 
-def get_data_create_account():
-    name=str(input("Enter your name: "))
-    address=str(input("Enther your address: "))
-    pin_number=str(input("Enter new pin number: "))
-    return(name,address,pin_number)
-
-       #===CREATE NEW ACCOUNT NUMBER====
+#===CREATE NEW ACCOUNT NUMBER====
 def create_account_number():
-    with open("customer_account.txt", "r") as customer_file:
-        return f"{int(customer_file.readlines()[-1].split(",")[0][1:])+1}"
+    with open("customer_accounts.txt", "r") as customers_file:
+        return f"{int(customers_file.readlines()[-1].split(",")[0])+1}"
     
-       #=====DEPOSIT FUNCTION=====
+#=====DEPOSIT FUNCTION=====
 def deposit():
     global ac_balances
-    try:
-        depAmount=float(input("Enter the deposit amount: "))
-        if depAmount > 0:
-            ac_balances= ac_balances + depAmount
-            print("Successfull deposit! Your depost amount is: ", depAmount, "Now your balance is: ", ac_balances)
-        else:
-            print("Invalid deposit amount. Must be greater than 0.")
-    except ValueError:
-        print("Invalid Input..")            
+    while True:
+        try:
+            depAmount=float(input("Enter The Deposit Amount: "))
+            if depAmount > 0:
+                ac_balances= ac_balances + depAmount
+                print("✅ Successfull deposit! Your Depost Amount Is: ", depAmount, "Now Your Balance Is: ", ac_balances)
+            else:
+                print("💴 Invalid Deposit Amount. Must Be Greater Than 0.")
+                break
+        except ValueError:
+            print("❌ Invalid Input..")
+            continue
+        return ac_balances           
 
-        #=====WITHDRAWAL FUNCTION=====
+#=====WITHDRAWAL FUNCTION=====
 def Withdrawal():
-    #global ac_balances
-    try:
-        withAmount=float(input("Enter the withdrawal amount: "))
-        if withAmount <= 0:
-            print("Invalid widhrawal amount. Must be greater than 0.")
-        elif withAmount <= ac_balances:
-            ac_balances= ac_balances - withAmount
-            print("Successfull withdrawal! Your withdrawal amount is: ", withAmount, "Now your balance is: ", ac_balances)
-        else:
-            print("Insufficien balance.")
-    except ValueError:
-        print("Invalid input..")
-
-def transaction():
-    with open("transactions.txt", "a") as transactions_file:
-        transactions_file.write()
-
+    global ac_balances
+    while True:
+        try:
+            withAmount=float(input("Enter The Withdrawal Amount: "))
+            if withAmount <= 0:
+                print("💴 Invalid Widhrawal Amount. Must Be Greater Than 0.")
+            elif withAmount <= ac_balances:
+                ac_balances= ac_balances - withAmount
+                print("✅ Successfull Withdrawal! Your Withdrawal Amount Is: ", withAmount, "Now Your Balance Is: ", ac_balances)
+                break
+            else:
+                print("🪫💵Insufficien Balance.")
+                continue
+        except ValueError:
+            print("❌ Invalid Input..")
+            continue
+        return ac_balances
 
 #=====CREATE CUSTOMER ACCOUNT=====
 def create_customer_account():
-    get_data_create_account()
-    create_account_number()
+    print("........Customer Account Creation Menue.......")
+    ac_num=create_account_number()
+    name=str(input("Enter Your Name: "))
+    address=str(input("🏡 Enter Your Address: "))
+    password=str(input("🗝️ Enter New Password: "))
     deposit()
-    with open("customer_account.txt", "a") as customer_file:
-        customer_file.writelines f"{create_account_number()},{get_data_create_account(name)},
-        {get_data_create_account(address)},{get_data_create_account(pin_number)},{deposit(depAmount)}"
-
+    with open("customer_accounts.txt", "a") as customers_file:
+        customers_file.writelines(f"{str(ac_num)},{name},{address},{password},{ac_balances}\n")
     with open("transactions.txt", "a") as transactions_file:
-        transactions_file.write(f"{create_account_number()}, D ,{deposit(depAmount)},{now}")    
+        transactions_file.write(f"{ac_num}, Dip ,{ac_balances},{now}\n")
 
-create_customer_account()     
+#======MAIN MENUE===========
+def main_menue():
+    while True:
+            print("========Welcome To The Banking Application======")
+            print("...............This Is Main Menue............")
+            print("1️⃣ For Admin Menue")
+            print("2️⃣ For Customer Menue")
+            print("3️⃣ For Exit")
+            choose = input("Enter your choose (1,2,3): ")
+            if choose == "1":
+                admin_login()
+            elif choose == "2":
+                customer_login()
+            elif choose == "3":       
+                print("🤝 Thank You For Choosing Our Service! ❤️")
+                break
+            else:
+                print("❌ Invalid Choose! Please Choose 1-3...")
+        
+#======ADMIN MENUE===========
+def admin_login():
+    get_admin_user_name = str(input("Enter The Admin User Name: "))
+    get_admin_password = str(input("🗝️ Enter The Admin Password: "))
+
+    while True:
+        if  admin_user_name == get_admin_user_name and admin_password == get_admin_password:
+            print("========Welcome To The Banking Application======")
+            print("...............This Is Admin's Menue............")
+            print("1️⃣ For Create Account")
+            print("2️⃣ For Deposit Money")
+            print("3️⃣ For Withdrawal Money")
+            print("4️⃣ For Check Balance")
+            print("5️⃣ For Transaction History")
+            print("6️⃣ For Exit")
+            choose = input("Enter your choose (1,2,3,4,5,6): ")
+            if choose == "1":
+                create_customer_account()
+            elif choose == "2":
+                deposit()
+            elif choose == "3":
+                Withdrawal()
+            elif choose == "4":
+                print("Check balance")
+            elif choose == "5":
+                print("Transaction History") 
+            elif choose == "6":       
+                print("🤝 Thank You For Choosing Our Service! ❤️")
+                break
+            else:
+                print("❌ Invalid Choose! Please Choose 1-6...")
+        else:
+                print("❌ Incorrect Username or password! Please Enter The Correct Username and Password.")
+
+#========CUSTOMER MENUE========
+def customer_login():
+    get_customer_account_number = str(input("Enter The Account Number: "))
+    get_customer_password = str(input("🗝️ Enter The Account Password: "))
+
+    while True:
+        if  admin_user_name == get_customer_account_number and admin_password == get_customer_password:
+            print("========Welcome To The Banking Application======")
+            print("...............This Is Customer's Menue............")
+            print("1️⃣ For Deposit Money")
+            print("2️⃣ For Withdrawal Money")
+            print("3️⃣ For Check Balance")
+            print("4️⃣ For Transaction History")
+            print("5️⃣ For Exit")
+            choose = input("Enter your choose (1,2,3,4,5): ")
+            if choose == "1":
+                deposit()
+            elif choose == "2":
+                Withdrawal()
+            elif choose == "3":
+                print("Check balance")
+            elif choose == "4":
+                print("Transaction History") 
+            elif choose == "5":       
+                print("🤝 Thank You For Choosing Our Service! ❤️")
+                break
+            else:
+                print("❌ Invalid Choose! Please Choose 1-5...")
+        else:
+                print("❌ Incorrect Username or password! Please Enter The Correct Username and Password.")                        
+
+#create_customer_account()     
 
         
